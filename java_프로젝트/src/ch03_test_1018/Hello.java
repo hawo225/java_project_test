6ed1f6c2a803910ee4789ce2c361af618edcb2be
hawo225 <hawo9910@naver.com>
2022-11-24 12:23:18 +0900

    저장1 test

---
+++ java_프로젝트/src/ch03_test_1018/Hello.java
@@ -0,0 +1,14 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+//	컨트롤 슬레쉬 주석
+		
+		// TODO Auto-generated method stub
+        System.out.println("hello 하승우 자바");
+        // 실행 ctrl+f11
+	}
+
+}
